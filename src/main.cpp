f14e53154349ba31218b1671820444201cf22cbc
Xertes0 <55015543+Xertes0@users.noreply.github.com>
2021-11-29 20:44:07 +0100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 
 #include <array>
+#include <cstdint>
 
 constexpr uint32_t SLEEP_MS{50};
 
@@ -15,7 +16,7 @@ main() -> int
 {
 	stdio_init_all();
 
-	for(auto const &pin : PIN_ARRAY) {
+	for(auto const& pin : PIN_ARRAY) {
 		gpio_init(pin);
 		gpio_set_dir(pin, GPIO_IN);
 		gpio_pull_up(pin);
@@ -23,7 +24,7 @@ main() -> int
 
 	char buffer[PIN_ARRAY.size()+1] = {'\0'};
 	while(true) {
-		for(size_t i=0; auto const &pin : PIN_ARRAY) {
+		for(size_t i=0; auto const& pin : PIN_ARRAY) {
 			auto const status = !static_cast<bool>(gpio_get(pin));
 			buffer[i] = status?'1':'0';
 			i++;
